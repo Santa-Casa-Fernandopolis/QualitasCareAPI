9a2d91b8c75a65167d866ebad5ad0f9c528c36f0
jeffersonarpasserini <jefferson.passerini@gmail.com>
2025-10-30 10:29:47 -0300

    projeto criado

---
+++ src/main/java/com/erp/qualitascareapi/QualitasCareApiApplication.java
@@ -0,0 +1,13 @@
+package com.erp.qualitascareapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualitasCareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualitasCareApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erp/qualitascareapi/QualitasCareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.qualitascareapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualitasCareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
