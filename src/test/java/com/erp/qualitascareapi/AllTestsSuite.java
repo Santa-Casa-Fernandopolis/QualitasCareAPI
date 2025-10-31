0317a465e716fbeda6e3914881b2343769889146
Jefferson Passerini <46405189+jeffersonarpasserini@users.noreply.github.com>
2025-10-31 20:25:58 -0300

    Add JUnit platform suite to execute all tests

---
+++ src/test/java/com/erp/qualitascareapi/AllTestsSuite.java
@@ -0,0 +1,10 @@
+package com.erp.qualitascareapi;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages("com.erp.qualitascareapi")
+public class AllTestsSuite {
+    // Executa todos os testes dentro do pacote com.erp.qualitascareapi
+}
